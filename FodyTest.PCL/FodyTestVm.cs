97de0ef065d9260fa639240910926b0975e92f06
Julien Rosen <jlrosen416@gmail.com>
2017-10-12 10:58:48 -0400

    Add project files.

---
+++ FodyTest.PCL/FodyTestVm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PropertyChanged;
+
+namespace FodyTest.PCL
+{
+    [AddINotifyPropertyChangedInterface]
+    public class FodyTestVm
+    {
+        public DateTime LabelText { get; set; } = DateTime.Now;
+
+        public FodyTestVm()
+        {
+            UpdateDate();
+        }
+
+        private async void UpdateDate()
+        {
+            while (true)
+            {
+                LabelText = DateTime.Now;
+
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
---
+++ FodyTest/FodyTest/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PropertyChanged;
+using Xamarin.Forms;
+using System.Threading;
+using FodyTest.PCL;
+
+namespace FodyTest
+{
+    [AddINotifyPropertyChangedInterface]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new FodyTestVm();
+        }
+    }
+}
